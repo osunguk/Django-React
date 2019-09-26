1d7f62e2728f00a9ae6d22531d122e56ae43d28f
osunguk <osunguk@gmail.com>
2019-09-26 16:36:28 +0900

    기본페이지 django 형식을 home url 추가

--- backend/djangoreactapi/djangoreactapi/urls.py
+++ backend/djangoreactapi/djangoreactapi/urls.py
@@ -1,8 +1,10 @@
 #backend/djangoreactapi/urls.py
 from django.contrib import admin
 from django.urls import path, include
+import post.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('post.urls')),
+    path('', post.views.home, name='home'),
 ]
--- backend/djangoreactapi/post/views.py
+++ backend/djangoreactapi/post/views.py
@@ -5,10 +5,16 @@ from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
+
 class ListPost(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
 class DetailPost(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+def home(request):
+    return render(request, 'app/home.html')
